803701a045382d10f5c9e8fe5d7f6a81ff86ec39
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-09-17 00:57:06 +0000

    card deck

--- src/app.js
+++ src/app.js
@@ -5,7 +5,49 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Write your code here:
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const btn = document.getElementById("generateCard");
+
+  btn.addEventListener("click", function() {
+    cardGenerator();
+  });
+};
+
+const cardGenerator = () => {
+  let suits = ["♦", "♣", "♥", "♠"];
+  let values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
+
+  document.querySelector(".top-suit").textContent = randomSuit;
+  document.querySelector(".rank").textContent = randomValue;
+  document.querySelector(".bottom-suit").textContent = randomSuit;
+
+  const redSuits = ["♥", "♦"];
+  if (redSuits.includes(randomSuit)) {
+    document.querySelector(".top-suit").style.color = "red";
+    document.querySelector(".bottom-suit").style.color = "red";
+    document.querySelector(".rank").style.color = "red";
+  } else {
+    document.querySelector(".top-suit").style.color = "black";
+    document.querySelector(".bottom-suit").style.color = "black";
+    document.querySelector(".rank").style.color = "black";
+  }
 };
